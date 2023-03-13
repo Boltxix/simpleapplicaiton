73bc0f2ae1a471a14d10de5a8088982d1a4306e0
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-13 20:54:37 +0000

    Fixing error in the deployment

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
-# Stop any instances of out application running currently
-pm2 stop simpleapplicaiton
 # Change directory iunto folder where application is downlaoded
 cd simpleapplicaiton/
+# Stop any instances of out application running currently
+pm2 stop simpleapplicaiton
 # Install application dependecies
+npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-npm install
 # start the application with the process name simpleapplicaiton using pm2
 pm2 start ./bin/www -- name simpleapplicaiton
